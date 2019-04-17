948c286109457f714fd2608abccbe6fa164625da
Sebastian Haas <sebastian@tapkey.com>
2019-04-17 12:45:22 +0200

    fixed proxy configuration

--- application.py
+++ application.py
@@ -27,7 +27,7 @@ if 'TAPKEY_BASE_URI' not in os.environ:
 app.secret_key = bytearray(os.environ.get('APP_SECRET_KEY'), encoding="utf-8")
 
 # Apply fix for https redirects behind a proxy (required for Azure only)
-app.wsgi_app = ProxyFix(app.wsgi_app)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_host=1)
 
 # Application Insights (required for Azure only)
 if 'APPINSIGHTS_INSTRUMENTATIONKEY' in os.environ:
